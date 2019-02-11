8e36c2e28a7996c2e7bfed1c15e1db27af3ffe6b
EngAhmed <ahmed.m.web.dev@gmail.com>
2019-02-12 01:32:37 +0400

    exporting connection

--- utils/db.con.js
+++ utils/db.con.js
@@ -1,9 +1,8 @@
 "use strict";
 const mongoose = require("mongoose");
 //create connection
-function Connection(DB_URL) {
+exports.DB_connection = (DB_URL) => {
     mongoose.connect(DB_URL, { useNewUrlParser: true, useCreateIndex: true })
         .then(console.log("Database connection Done!"))
         .catch((error) => console.log(error));
 }
-exports.DB_connection = Connection();
